1f4195b2c53ddfb8a823bb23287128867cfbe88c
sahar feferovich <sahar.fefer@gmail.com>
2020-11-10 13:16:59 +0200

    ready to cheack

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, render_template, redirect, json
 from flask_mysqldb import MySQL
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_HOST'] = 'localhost:3306'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '1234'
 app.config['MYSQL_DB'] = 'realtor'
@@ -18,15 +19,9 @@ def Home():
 
 
 @app.route('/apartments', methods=['GET'])
-# def getApartments():
-#     cur = mysql.connection.cursor()
-#     cur.execute('''SELECT * FROM apartments''')
-#     results = cur.fetchall()
-#     return jsonify(results)
-
 def getApartments():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM apartments''')
+    cur.execute('''Select * from apartments where status = "approved" and availability  = "available"''')
     results = cur.fetchall()
     return jsonify(results)
 
@@ -46,43 +41,26 @@ def getApartmentsUser(user_id):
     results = cur.fetchall()
     return jsonify(results)
 
+@app.route('/apartments/rent_or_sale', methods=['GET'])
+def getRentSaleApartments():
+    sale = getSaleApartments()
+    rent = getRentApartments()
+    print('sale', sale)
+    print('rent', rent)
+    print({"rent": rent, "sale": sale})
+    return {"rent": rent, "sale": sale}
 
-####### not working #######
-@app.route('/<user_id>/<status>', methods=['GET'])
-def getApartmentsUserByStatus(user_id, status):
+def getSaleApartments():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM apartments WHERE user_id = (%s) AND status = (%s)''' % (user_id, status))
+    cur.execute('''SELECT COUNT(*) as sale FROM apartments where status = 'approved' and availability = 'available' and sale_status = 'sale';''')
     results = cur.fetchall()
-    return jsonify(results)
+    return results[0][0]
 
-
-####### send only sale apartments #######
-# @app.route('/apartments/rent_or_sale', methods=['GET'])
-# def getRentSaleApartments():
-#     saleApartments = getSaleApartments()
-#     rentApartments = getRentApartments()
-#     return saleApartments
-
-###### not working - try to join two arrs to one #######
-# @app.route('/apartments/rent_or_sale', methods=['GET'])
-# def getRentSaleApartments():
-#     saleApartments = getSaleApartments()
-#     rentApartments = getRentApartments()
-#     rentSaleApartments = {rentApartments + saleApartments}
-#     return rentSaleApartments
-
-###### working #######
-# def getSaleApartments():
-#     cur = mysql.connection.cursor()
-#     cur.execute('''SELECT COUNT(*) as sale FROM apartments where status = 'approved' and availability = 'available' and sale_status = 'sale';''')
-#     results = cur.fetchall()
-#     return jsonify(results)
-#
-# def getRentApartments():
-#     cur = mysql.connection.cursor()
-#     cur.execute('''SELECT COUNT(*) as rent FROM apartments where status = 'approved' and availability = 'available' and sale_status = 'rent';''')
-#     results = cur.fetchall()
-#     return jsonify(results)
+def getRentApartments():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT COUNT(*) as rent FROM apartments where status = 'approved' and availability = 'available' and sale_status = 'rent';''')
+    results = cur.fetchall()
+    return results[0][0]
 
 @app.route('/cities', methods=['GET'])
 def getCities():
@@ -107,26 +85,27 @@ def allCitiesWithApartments():
     results = cur.fetchall()
     return jsonify(results)
 
-
-# @app.route('/bla')
-# def bla():
-#     cur = mysql.connection.cursor()
-#     cur.execute('''SELECT * FROM users''')
-#     rv = cur.fetchall()
-#     return str(rv)
-
-# @app.route('/<user>')
-# def hello_user(user):
-#     return "Hello {}!".format(user)
-#
-# @app.route('/len/<word>')
-# def len_count(word):
-#     return 'word len = {}#'.format(str(len(word)))
-
+@app.route("/users/login", methods=['POST', 'OPTIONS'])
+def login():
+    user_data = request.get_json()
+    email = user_data['email']
+    password = user_data['password']
+    if email and password:
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password,))
+        user = cursor.fetchone()
+        if user:
+            res = make_response('send a cookie')
+            user_ditails = {'id': user[0], 'first_name': user[2]}
+            print('json.dumps', json.dumps(user_ditails))
+            res.set_cookie('auth', str(user_ditails), max_age = 60 * 60 * 24 * 365 * 2)
+            return res
+        else:
+            return 'Incorrect username/password!'
+    else:
+        return 'nooo'
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=4040, debug=True)
 
 app.run()
-
-# print('hello!')
